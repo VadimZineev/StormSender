de17da1a02dde533090e41d50c095acdd174c940
VadimZineev <vadimz026@gmail.com>
2026-03-10 22:20:51 +0300

    start storm

---
+++ src/main/java/com/pulse/stormsender/StormsenderApplication.java
@@ -0,0 +1,13 @@
+package com.pulse.stormsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormsenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StormsenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulse/stormsender/StormsenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulse.stormsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StormsenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
