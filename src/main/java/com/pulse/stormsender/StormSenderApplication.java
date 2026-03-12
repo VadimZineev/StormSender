fc7d6b991aa7b29c893a8aec7036f933b962b1e0
VadimZineev <vadimz026@gmail.com>
2026-03-13 00:06:22 +0300

    SSP-1: Настройка проекта

--- src/main/java/com/pulse/stormsender/StormSenderApplication.java
+++ src/main/java/com/pulse/stormsender/StormSenderApplication.java
@@ -2,7 +2,6 @@ package com.pulse.stormsender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class StormSenderApplication {
 
--- src/test/java/com/pulse/stormsender/StormsenderApplicationTests.java
+++ src/test/java/com/pulse/stormsender/StormsenderApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StormsenderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
